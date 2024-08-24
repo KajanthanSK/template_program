dbaf9a54deba790b89259e9ec68c2d3e22e8a68b
KajanthanSK <kajanthan@spil.com.au>
2024-08-24 23:20:19 +0530

    class template

--- TemplateProgram.cpp
+++ TemplateProgram.cpp
@@ -8,6 +8,16 @@ T add(T a, T b)
 	return (a + b);
 }
 
+template <typename T>
+class Box
+{
+private:
+	T value;
+public:
+	void setValue(T v) { value = v; }
+	T getValue() { return value; }
+};
+
 int main()
 {
 	int x = 5, y = 6;
@@ -16,5 +26,36 @@ int main()
 	cout << add(x, y) << endl; // Calls add<int>
 	cout << add(p, q) << endl; // Calls add<double>
 
+	Box<int> intBox;
+	intBox.setValue(10);
+	cout << intBox.getValue() << endl;
+
+	Box<double> doubleBox;
+	doubleBox.setValue(10.5);
+	cout << doubleBox.getValue() << endl;
+
 	return 0;
 }
+
+//template <typename T>
+//class Box
+//{
+//private:
+//	T value;
+//public:
+//	void setValue(T v) { value = v; }
+//	T getValue() { return value; }
+//};
+//
+//int main()
+//{
+//	Box<int> intBox;
+//	intBox.setValue(10);
+//	cout << intBox.getValue() << endl;
+//
+//	Box<double> doubleBox;
+//	doubleBox.setValue(10.5);
+//	cout << doubleBox.getValue() << endl;
+//
+//	return 0;
+//}
